9fbd4539a3415fcd0f101808f6242020134d0dbe
eldenhowes <williameldenhowes@gmail.com>
2019-10-01 12:17:14 +0100

    Refactored to include a separate function for creating a hash for each weight

--- lib/sum_of_digits_order.rb
+++ lib/sum_of_digits_order.rb
@@ -1,9 +1,9 @@
 def sum_of_digits_order(weights)
   weights_array_of_hashes = weights_array_of_hashes(weights)
-  sorted_array = weights_array_of_hashes.sort_by { |weight| [ weight['sum of digits'], weight['weight'] ] }
+  sorted_array = weights_array_of_hashes.sort_by { |weight| [ weight[:sum_of_digits], weight[:weight] ] }
   result = []
   sorted_array.each do |weight_hash| 
-    result << weight_hash['weight']
+    result << weight_hash[:weight]
   end
   result.join(' ')
 end
@@ -17,10 +17,14 @@ end
 def weights_array_of_hashes(weights)
   weights_array_of_hashes = []
   weights.split(' ').each do |weight|
-    weights_hash = Hash.new
-    weights_hash["weight"] = weight
-    weights_hash["sum of digits"] = sum_of_digits(weight)
-    weights_array_of_hashes << weights_hash
+    weights_array_of_hashes << create_hash(weight)
   end
   weights_array_of_hashes
+end
+
+def create_hash(weight)
+  weights_hash = Hash.new
+  weights_hash[:weight] = weight
+  weights_hash[:sum_of_digits] = sum_of_digits(weight)
+  weights_hash
 end
