83907090fee46b2995d8e66605110ac65aa8d056
eldenhowes <williameldenhowes@gmail.com>
2019-10-01 12:05:20 +0100

    Now handles when two of the numbers are equal in digit value but digits are not the same. These are now sorted by string value

--- lib/sum_of_digits_order.rb
+++ lib/sum_of_digits_order.rb
@@ -1,12 +1,30 @@
 def sum_of_digits_order(weights)
-  weights.split(' ').sort_by { |weight| sum_of_digits(weight.to_i)}.join(' ')
+  weights_array_of_hashes = weights_array_of_hashes(weights)
+  sorted_array = weights_array_of_hashes.sort_by { |weight| [ weight['sum of digits'], weight['weight'] ] }
+  result = []
+  sorted_array.each do |weight_hash| 
+    result << weight_hash['weight']
+  end
+  result.join(' ')
 end
 
 private
 
 def sum_of_digits(number)
-  no_of_100s = number / 100
+  no_of_1000s = number / 1000
+  no_of_100s = number % 1000 / 100
   no_of_10s = number % 100 / 10
   no_of_1s = number % 10 / 1
   sum = no_of_100s + no_of_10s + no_of_1s
+end
+
+def weights_array_of_hashes(weights)
+  weights_array_of_hashes = []
+  weights.split(' ').each do |weight|
+    weights_hash = Hash.new
+    weights_hash["weight"] = weight
+    weights_hash["sum of digits"] = sum_of_digits(weight.to_i)
+    weights_array_of_hashes << weights_hash
+  end
+  weights_array_of_hashes
 end
--- spec/sum_of_digits_order_spec.rb
+++ spec/sum_of_digits_order_spec.rb
@@ -5,13 +5,15 @@ describe '#sum_of_digits_order' do
     expect(sum_of_digits_order('9 10')).to eq('10 9')
   end
 
-  it 'correctly handles an string of 9 10 101' do
+  it 'correctly handles a string of 9 10 101' do
     expect(sum_of_digits_order('9 10 101')).to eq('10 101 9')
   end
 
-  it 'correctly handles an string of 9 55 105 201' do
+  it 'correctly handles a string of 9 55 105 201' do
     expect(sum_of_digits_order('9 55 105 201')).to eq('201 105 9 55')
   end
 
-
+  it 'correctly handles a string where two numbers are equal but digits different' do
+    expect(sum_of_digits_order("56 65 74 100 99 68 86 180 90")).to eq("100 180 90 56 65 74 68 86 99")
+  end
 end
